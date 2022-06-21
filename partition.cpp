79493aab417e95a546f88472973a09137c7e23b3
Egor Pilipenko <pilipenko0409@gmail.com>
2022-06-21 13:36:50 +0700

    Fix /data encryption and formatting for some redmi/realme devices launched with Android 10
    Change-Id: I8921f75013b41ab7659dfb2f21f0e8c0d3ada349

--- partition.cpp
+++ partition.cpp
@@ -691,10 +691,8 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 	UnMount(false);
 
 #ifdef TW_INCLUDE_CRYPTO
-	#ifdef TW_PREPARE_DATA_MEDIA_EARLY
 	if (datamedia)
 		Setup_Data_Media();
-	#endif
 	Can_Be_Encrypted = true;
 	char crypto_blkdev[255];
 	property_get("ro.crypto.fs_crypto_blkdev", crypto_blkdev, "error");
@@ -702,10 +700,6 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 		Set_FBE_Status();
 		Decrypted_Block_Device = crypto_blkdev;
 		LOGINFO("Data already decrypted, new block device: '%s'\n", crypto_blkdev);
-		#ifndef TW_PREPARE_DATA_MEDIA_EARLY
-		if (datamedia)
-			Setup_Data_Media();
-		#endif
 		DataManager::SetValue(TW_IS_ENCRYPTED, 0);
 	} else if (!Mount(false)) {
 		if (Is_Present) {
@@ -747,10 +741,6 @@ void TWPartition::Setup_Data_Partition(bool Display_Error) {
 				LOGERR("Unable to decrypt FBE device\n");
 		} else {
 			DataManager::SetValue(TW_IS_ENCRYPTED, 0);
-			#ifndef TW_PREPARE_DATA_MEDIA_EARLY
-			if (datamedia)
-				Setup_Data_Media();
-			#endif
 		}
 	}
 	if (datamedia && (!Is_Encrypted || (Is_Encrypted && Is_Decrypted))) {
@@ -1266,12 +1256,7 @@ void TWPartition::Setup_Data_Media() {
 			Make_Dir("/sdcard", false);
 			Symlink_Mount_Point = "/sdcard";
 		}
-		#ifdef TW_PREPARE_DATA_MEDIA_EARLY
 		if (Mount(false) && TWFunc::Path_Exists(Mount_Point + "/media/0")) {
-		#else
-		Mount(false);
-		if (TWFunc::Path_Exists(Mount_Point + "/media/0")) {
-		#endif
 			Storage_Path = Mount_Point + "/media/0";
 			Symlink_Path = Storage_Path;
 			DataManager::SetValue(TW_INTERNAL_PATH, Mount_Point + "/media/0");
@@ -1983,6 +1968,13 @@ bool TWPartition::Repair() {
 		Find_Actual_Block_Device();
 		command = "/system/bin/fsck.f2fs " + Actual_Block_Device;
 		LOGINFO("Repair command: %s\n", command.c_str());
+		if (Mount_Point == "/data") {
+			LOGINFO("Bind-unmounting /sdcard before f2fs data repair...\n");
+			usleep(32768);
+			string nul;
+			TWFunc::Exec_Cmd("umount /sdcard", false);
+			usleep(32768);
+		}
 		if (TWFunc::Exec_Cmd(command) == 0) {
 			gui_msg("done=Done.");
 			return true;
@@ -2558,6 +2550,12 @@ bool TWPartition::Wipe_F2FS() {
 		NeedPreserveFooter = false;
 	}
 	LOGINFO("mkfs.f2fs command: %s\n", f2fs_command.c_str());
+		if (Mount_Point == "/data") {
+			LOGINFO("Bind-unmounting /sdcard before f2fs data format...\n");
+			usleep(32768);
+			TWFunc::Exec_Cmd("umount /sdcard", false);
+			usleep(32768);
+		}
 	if (TWFunc::Exec_Cmd(f2fs_command) == 0) {
 		if (NeedPreserveFooter)
 			Wipe_Crypto_Key();
